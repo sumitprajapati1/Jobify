1269b9a30d7bcb117fc0ddb7a70e17f1b444e344
sumit <prajapatisumit009@gmail.com>
2025-07-28 18:31:20 +0530

    add proper frontend url

--- backend/app.js
+++ backend/app.js
@@ -12,17 +12,12 @@ import { errorMiddleware } from "./middlewares/error.js";
 const app = express();
 dotenv.config({path:"./config/config.env"})
 
-// app.use(cors({
-//     origin: [process.env.FRONTEND_URL],
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true,
-// }))
-
 app.use(cors({
-    origin: "*",
+    origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-}));
+}))
+
 
 app.use(cookieParser());
 app.use(express.json());
